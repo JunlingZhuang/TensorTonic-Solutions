2ed692ff743bb586c64ae7e9c8cdee40193134cf
ZHUANG JUNLING <108850976+JunlingZhuang@users.noreply.github.com>
2026-05-25 23:03:02 -0400

    Add solution for sigmoid-numpy

---
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def sigmoid(x):
+    """
+    Vectorized sigmoid function.
+    """
+    # Write code here
+
+    x_arr = np.asarray(x, dtype = float)
+
+    z = 1.0 / (1.0 + np.exp(-x_arr))
+    
+    return z
